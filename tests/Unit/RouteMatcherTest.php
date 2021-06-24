6503eace06712a6e9d8b62a8dc48ff62410b4a65
Dominik Zogg <dominik.zogg@hotmail.com>
2021-06-24 11:30:51 +0200

    Add RouteMatcherTest

---
+++ tests/Unit/RouteMatcherTest.php
@@ -0,0 +1,169 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chubbyphp\Tests\Framework\Router\HackRouting\Unit;
+
+use Chubbyphp\Framework\Router\HackRouting\RouteMatcher;
+use Chubbyphp\Framework\Router\Route;
+use Chubbyphp\Framework\Router\RouterException;
+use Chubbyphp\Framework\Router\Routes;
+use Chubbyphp\Mock\Call;
+use Chubbyphp\Mock\MockByCallsTrait;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * @covers \Chubbyphp\Framework\Router\HackRouting\RouteMatcher
+ *
+ * @internal
+ */
+final class RouteMatcherTest extends TestCase
+{
+    use MockByCallsTrait;
+
+    const UUID_PATTERN = '[0-9a-f]{8}\b-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-\b[0-9a-f]{12}';
+
+    public function testMatchFound(): void
+    {
+        /** @var RequestHandlerInterface|MockObject $requestHandler1 */
+        $requestHandler1 = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route1 = Route::post('/api/pets', 'pet_create', $requestHandler1);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler2 */
+        $requestHandler2 = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route2 = Route::get('/api/pets', 'pet_list', $requestHandler2);
+
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getPath')->with()->willReturn('/api/pets'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getMethod')->with()->willReturn('GET'),
+            Call::create('getUri')->with()->willReturn($uri),
+        ]);
+
+        $routeMatcher = new RouteMatcher(new Routes([$route1, $route2]));
+
+        self::assertSame($route2->getName(), $routeMatcher->match($request)->getName());
+    }
+
+    public function testMatchNotFound(): void
+    {
+        $this->expectException(RouterException::class);
+        $this->expectExceptionMessage(
+            'The page "/" you are looking for could not be found.'
+                .' Check the address bar to ensure your URL is spelled correctly.'
+        );
+        $this->expectExceptionCode(404);
+
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getPath')->with()->willReturn('/'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getMethod')->with()->willReturn('GET'),
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getRequestTarget')->with()->willReturn('/'),
+        ]);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/api/pets', 'pet_list', $requestHandler);
+
+        $routeMatcher = new RouteMatcher(new Routes([$route]));
+        $routeMatcher->match($request);
+    }
+
+    public function testMatchMethodNotAllowed(): void
+    {
+        $this->expectException(RouterException::class);
+        $this->expectExceptionMessage(
+            'Method "POST" at path "/api/pets?offset=1&limit=20" is not allowed. Must be one of: "GET"'
+        );
+        $this->expectExceptionCode(405);
+
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getPath')->with()->willReturn('/api/pets'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getMethod')->with()->willReturn('POST'),
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getRequestTarget')->with()->willReturn('/api/pets?offset=1&limit=20'),
+        ]);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/api/pets', 'pet_list', $requestHandler);
+
+        $routeMatcher = new RouteMatcher(new Routes([$route]));
+        $routeMatcher->match($request);
+    }
+
+    public function testMatchWithTokensNotMatch(): void
+    {
+        $this->expectException(RouterException::class);
+        $this->expectExceptionMessage(
+            'The page "/api/pets/1" you are looking for could not be found.'
+                .' Check the address bar to ensure your URL is spelled correctly.'
+        );
+        $this->expectExceptionCode(404);
+
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getPath')->with()->willReturn('/api/pets/1'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getMethod')->with()->willReturn('GET'),
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getRequestTarget')->with()->willReturn('/api/pets/1'),
+        ]);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/api/pets/{id:'.self::UUID_PATTERN.'}', 'pet_read', $requestHandler);
+
+        $routeMatcher = new RouteMatcher(new Routes([$route]));
+        $routeMatcher->match($request);
+    }
+
+    public function testMatchWithTokensMatch(): void
+    {
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getPath')->with()->willReturn('/api/pets/8b72750c-5306-416c-bba7-5b41f1c44791'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getMethod')->with()->willReturn('GET'),
+            Call::create('getUri')->with()->willReturn($uri),
+        ]);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/api/pets/{id:'.self::UUID_PATTERN.'}', 'pet_read', $requestHandler);
+
+        $routeMatcher = new RouteMatcher(new Routes([$route]));
+
+        self::assertSame($route->getName(), $routeMatcher->match($request)->getName());
+    }
+}
