f9acb43d6f0ca29f2ddb47d0a9fa6622abe010bd
Dominik Zogg <dominik.zogg@hotmail.com>
2021-06-24 22:37:42 +0200

    Add some tests

--- tests/Unit/UrlGeneratorTest.php
+++ tests/Unit/UrlGeneratorTest.php
@@ -125,4 +125,130 @@ final class UrlGeneratorTest extends TestCase
         $router = new UrlGenerator(new Routes([$route]));
         $router->generateUrl($request, 'user', ['id' => 'a3bce0ca-2b7c-4fc6-8dad-ecdcc6907791']);
     }
+
+    public function testGenerateUriWithBasePath(): void
+    {
+        /** @var MockObject|UriInterface $uri */
+        $uri = $this->getMockByCalls(UriInterface::class, [
+            Call::create('getScheme')->with()->willReturn('https'),
+            Call::create('getAuthority')->with()->willReturn('user:password@localhost'),
+            Call::create('getScheme')->with()->willReturn('https'),
+            Call::create('getAuthority')->with()->willReturn('user:password@localhost'),
+            Call::create('getScheme')->with()->willReturn('https'),
+            Call::create('getAuthority')->with()->willReturn('user:password@localhost'),
+            Call::create('getScheme')->with()->willReturn('https'),
+            Call::create('getAuthority')->with()->willReturn('user:password@localhost'),
+        ]);
+
+        /** @var MockObject|ServerRequestInterface $request */
+        $request = $this->getMockByCalls(ServerRequestInterface::class, [
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getUri')->with()->willReturn($uri),
+            Call::create('getUri')->with()->willReturn($uri),
+        ]);
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/user/{id:\d+}[/{name}]', 'user', $requestHandler);
+
+        $router = new UrlGenerator(new Routes([$route]), null, '/path/to/directory');
+
+        self::assertSame(
+            'https://user:password@localhost/path/to/directory/user/1',
+            $router->generateUrl($request, 'user', ['id' => 1])
+        );
+        self::assertSame(
+            'https://user:password@localhost/path/to/directory/user/1?key=value',
+            $router->generateUrl($request, 'user', ['id' => 1], ['key' => 'value'])
+        );
+        self::assertSame(
+            'https://user:password@localhost/path/to/directory/user/1/sample',
+            $router->generateUrl($request, 'user', ['id' => 1, 'name' => 'sample'])
+        );
+        self::assertSame(
+            'https://user:password@localhost/path/to/directory/user/1/sample?key1=value1&key2=value2',
+            $router->generateUrl(
+                $request,
+                'user',
+                ['id' => 1, 'name' => 'sample'],
+                ['key1' => 'value1', 'key2' => 'value2']
+            )
+        );
+    }
+
+    public function testGeneratePathWithMissingRoute(): void
+    {
+        $this->expectException(RouterException::class);
+        $this->expectExceptionMessage('Missing route: "user"');
+        $this->expectExceptionCode(1);
+
+        $router = new UrlGenerator(new Routes([]));
+        $router->generatePath('user', ['id' => 1]);
+    }
+
+    public function testGeneratePath(): void
+    {
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/user/{id:\d+}[/{name}]', 'user', $requestHandler);
+
+        $router = new UrlGenerator(new Routes([$route]));
+
+        self::assertSame('/user/1', $router->generatePath('user', ['id' => 1]));
+        self::assertSame('/user/1?key=value', $router->generatePath('user', ['id' => 1], ['key' => 'value']));
+        self::assertSame('/user/1/sample', $router->generatePath('user', ['id' => 1, 'name' => 'sample']));
+        self::assertSame(
+            '/user/1/sample?key1=value1&key2=value2',
+            $router->generatePath(
+                'user',
+                ['id' => 1, 'name' => 'sample'],
+                ['key1' => 'value1', 'key2' => 'value2']
+            )
+        );
+    }
+
+    public function testGeneratePathWithMissingAttribute(): void
+    {
+        $this->expectException(RouterException::class);
+        $this->expectExceptionMessage('Missing attribute "id" while path generation for route: "user"');
+
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/user/{id:\d+}[/{name}]', 'user', $requestHandler);
+
+        $router = new UrlGenerator(new Routes([$route]));
+        $router->generatePath('user');
+    }
+
+    public function testGeneratePathWithBasePath(): void
+    {
+        /** @var RequestHandlerInterface|MockObject $requestHandler */
+        $requestHandler = $this->getMockByCalls(RequestHandlerInterface::class);
+
+        $route = Route::get('/user/{id:\d+}[/{name}]', 'user', $requestHandler);
+
+        $router = new UrlGenerator(new Routes([$route]), null, '/path/to/directory');
+
+        self::assertSame('/path/to/directory/user/1', $router->generatePath('user', ['id' => 1]));
+        self::assertSame(
+            '/path/to/directory/user/1?key=value',
+            $router->generatePath('user', ['id' => 1], ['key' => 'value'])
+        );
+        self::assertSame(
+            '/path/to/directory/user/1/sample',
+            $router->generatePath('user', ['id' => 1, 'name' => 'sample'])
+        );
+        self::assertSame(
+            '/path/to/directory/user/1/sample?key1=value1&key2=value2',
+            $router->generatePath(
+                'user',
+                ['id' => 1, 'name' => 'sample'],
+                ['key1' => 'value1', 'key2' => 'value2']
+            )
+        );
+    }
 }
